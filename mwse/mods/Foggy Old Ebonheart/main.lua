94f4780c3d85ea3abee246c55e830883fb0fd71a
Nathaniel Williams <nathaniel.williams@coios.net>
2024-09-10 00:07:24 +0800

    Add configurable whitelist via JSON

--- mwse/mods/Foggy Old Ebonheart/main.lua
+++ mwse/mods/Foggy Old Ebonheart/main.lua
@@ -6,22 +6,36 @@ local defaultConfig = {
     viewRangeIncrementSteps = 10,
 }
 
-local configPath = "FoggyOldEbonheart"
+local defaultWhiteList = {
+    cells = {
+        "Old Ebonheart",
+        "Old Ebonheart, Docks"
+    }
+}
+
+local configPath = "config"
+local whitelistPath = "whitelist"
 
 local config = mwse.loadConfig(configPath, defaultConfig)
+local whitelist = mwse.loadConfig(whitelistPath, defaultWhiteList)
 
-local function isOldEbonheart(cell)
-    return cell.displayName == "Old Ebonheart" or cell.displayName == "Old Ebonheart, Docks"
+local function isReducedDrawDistanceCell(cell)
+    -- Check if cell.name is whitelisted
+    for _, cellName in ipairs(whitelist.cells) do
+        if cellName == cell.name then
+            return true
+        end
+    end
 end
 
 local function increaseViewRange()
-    for i=1, viewRangeIncrementSteps do
+    for i = 1, viewRangeIncrementSteps do
         mge.macros.increaseViewRange()
     end
 end
 
 local function decreaseViewRange()
-    for i=1, viewRangeIncrementSteps do
+    for i = 1, viewRangeIncrementSteps do
         mge.macros.decreaseViewRange()
     end
 end
@@ -29,21 +43,27 @@ end
 local function onGameLoaded(e)
     prevCell = tes3.player.cell
     mwse.log("[FOE] viewRangeIncrementSteps: %s", config.viewRangeIncrementSteps)
-    if isOldEbonheart(tes3.player.cell) then
+    for _, cellName in ipairs(whitelist.cells) do
+        mwse.log("[FOE] Whitelisted cell: %s", cellName)
+    end
+    if isReducedDrawDistanceCell(tes3.player.cell) then
         local prevRenderDistance = game.renderDistance
         decreaseViewRange()
-        mwse.log("[FOE] player is Old Ebonheart, decreased render distance from %s to %s", prevRenderDistance, game.renderDistance)
+        mwse.log("[FOE] player is in designated area, decreased render distance from %s to %s", prevRenderDistance,
+            game.renderDistance)
     end
 end
 
 local function onCellChanged(e)
     local prevRenderDistance = game.renderDistance
-    if(isOldEbonheart(e.cell) and (not isOldEbonheart(prevCell))) then -- Current cell is Old Ebonheart, previous cell is not
+    if (isReducedDrawDistanceCell(e.cell) and (not isReducedDrawDistanceCell(prevCell))) then -- Current cell is Old Ebonheart, previous cell is not
         decreaseViewRange()
-        mwse.log("[FOE] Entering Old Ebonheart, decreased render distance from %s to %s", prevRenderDistance, game.renderDistance)
-    elseif((not isOldEbonheart(e.cell)) and isOldEbonheart(prevCell)) then -- Current cell is not Old Ebonheart, previous cell is
+        mwse.log("[FOE] Entering designated area, decreased render distance from %s to %s", prevRenderDistance,
+            game.renderDistance)
+    elseif ((not isReducedDrawDistanceCell(e.cell)) and isReducedDrawDistanceCell(prevCell)) then -- Current cell is not Old Ebonheart, previous cell is
         increaseViewRange()
-        mwse.log("[FOE] Leaving Old Ebonheart, increased render distance from %s to %s", prevRenderDistance, game.renderDistance)
+        mwse.log("[FOE] Leaving designated area, increased render distance from %s to %s", prevRenderDistance,
+            game.renderDistance)
     end
     prevCell = e.cell
 end
